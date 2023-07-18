53e2e28e9a53270731a82773713ff12868a64cf3
Soham Mitra <mitra.soham53@gmail.com>
2023-07-19 00:35:50 +0530

    setting up hook forms for login and register

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,6 +1,25 @@
+import {useForm} from 'react-hook-form';
+import axios from 'axios';
+import {DevTool} from '@hookform/devtools';
+const url = 'http://localhost:4242/'
 const Login = () => {
+
+  const {register, handleSubmit, control} = useForm();
+
+  const onSubmit = (data) => {
+    axios.post('')
+  }
+
   return (
-    <div>Login</div>
+    <>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username</label>
+      <input type="text" name="username" id="uname" {...register("username")}/>
+      <label htmlFor="password">Password</label>
+      <input type="password" name="password" id="password" {...register("password")}/>
+    </form>
+    <DevTool control={control}/>
+    </>
   )
 }
 
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,6 +1,29 @@
+import {useForm} from 'react-hook-form';
+import axios from 'axios';
+import {DevTool} from '@hookform/devtools';
+const url = 'http://localhost:4242/'
 const Register = () => {
+
+  const {register, handleSubmit, control} = useForm();
+
+  const onSubmit = (data) => {
+    axios.post('')
+  }
+
   return (
-    <div>Register</div>
+    <>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Full Name</label>
+      <input type="text" name="name" id="name" {...register("name")}/>
+      <label htmlFor="username">Username</label>
+      <input type="text" name="username" id="uname" {...register("username")}/>
+      <label htmlFor="email">Email ID</label>
+      <input type="email" name="email" id="email" {...register("email")} />
+      <label htmlFor="password">Password</label>
+      <input type="password" name="password" id="password" {...register("password")}/>
+    </form>
+    <DevTool control={control}/>
+    </>
   )
 }
 
