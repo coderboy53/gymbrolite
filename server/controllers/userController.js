8e79368a81d65d07938eeca4e556aac66a9fe3dc
Soham Mitra <mitra.soham53@gmail.com>
2023-07-19 00:36:51 +0530

    set up user login and register controllers and initialized mongoose connection

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,15 @@
-const login = (req, res) => {
-    
-}
+const mongoose = require('mongoose');
+const mongoUser = process.env.USERNAME;
+const mongoPass = process.env.PASSWORD;
+const login = async (req, res) => {
+    await mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPass}@cluster0.ppcxe1f.mongodb.net/?retryWrites=true&w=majority`)
+};
+
+const register = async (req, res) => {
+    await mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPass}@cluster0.ppcxe1f.mongodb.net/?retryWrites=true&w=majority`)
+};
 
 module.exports = {
     login,
     register
-}
+};
