ba32333344ee47472fd6ed6f6e765626262e16d6
Soham Mitra <mitra.soham53@gmail.com>
2023-07-19 00:37:53 +0530

    setup the express routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded);
 const port = process.env.PORT || 5000;
+
+
 app.use('/user', require('./routes/userRoutes'));
+app.use('/workouts');
 
 
 
