a3f5786e3b760030250e6e9c08e47cf9b525a8be
Soham Mitra <mitra.soham53@gmail.com>
2023-07-20 15:18:26 +0530

    fixed missing listen on port and added mongodb connection function

--- server/server.js
+++ server/server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded);
+app.use(express.urlencoded({extended: true}));
 const port = process.env.PORT || 5000;
 
+const connect = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Connection started at ${conn.connection.host}`)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.listen(port, () => {console.log(`Server started at port: ${port}`)});
+
+connect();
 
 app.use('/user', require('./routes/userRoutes'));
-app.use('/workouts');
-
-
+// app.use('/workouts');
 
 
